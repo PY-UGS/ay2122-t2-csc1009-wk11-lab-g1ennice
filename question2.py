4170b25eb2bd538a1dbcf6c70e2de1e22354eabe
g1ennice <93643326+g1ennice@users.noreply.github.com>
2022-04-05 13:27:20 +0800

    Delete question2.py

--- question2.py
+++
@@ -1,4 +0,0 @@
-x = int(input("Please enter first number to add: ")) # get first number and store it as int
-y = int(input("Please enter second number to add: ")) # get second number and store it as int
-
-print(f"Average of the 2 numbers is: {round((x+y)/2,2)}") # add both numbers and divide by 2, then round to 2 decimal places.
